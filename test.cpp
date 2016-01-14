f41d701449ce0845bb907dedefdfc9b9680a39bf
Maxim Alekseykin <baxton-sg@users.noreply.github.com>
2016-01-14 20:07:57 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,166 +1,221 @@
 
 
+
 import os
 import sys
+import csv
 import numpy as np
 import scipy as sp
-from scipy.cluster.vq import vq, kmeans, whiten
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import dicom
 
+from sklearn.linear_model import LogisticRegression
 
-dir = ""
-fnames = None
-files_num = 0
 
-buffers = {}
 
-def set_cls(buffer, check, r, c, cls, counters):
-    if 0 == check[r,c]:
-        return False
+PATH_BASE = "..\\data\\"
+PATH_CACHE = "..\\cache\\"
 
-    if r > 0 and buffer[r-1,c] > 0:
-        buffer[r,c] = buffer[r-1,c]
-        counters[buffer[r,c]] += 1
-        return False
+
+
+POSITIONS = {}
+
+
+TRAIN_SET = []
+TEST_SET  = []
+
+
+BUFFERS = {}
+
+
+
+
+
+
+
+def detect_rectangle(buffer, start_r, start_c, visited):
+    R = buffer.shape[0]
+    C = buffer.shape[1]
+
+    top = start_r
+    bottom = top
+
+    left = start_c
+    right = left
     
+    S = 1
 
-    if c > 0 and buffer[r,c-1] > 0:
-        buffer[r,c] = buffer[r,c-1]
-        counters[buffer[r,c]] += 1
-        return False
+    key = (start_r, start_c)
+    Q = [key]
 
+    while 0 < len(Q):
+        key = Q.pop()
+        visited.add(key)
 
-    buffer[r,c] = cls
-    counters[cls] = 1
+        # top
+        if 0 < key[0]:
+           r = key[0] - 1
+           c = key[1]
+           if 0 < buffer[r,c]:
+               k = (r,c)
+               if not k in visited:
+                   S += 1
+                   visited.add(k)
+                   Q.append(k)
 
-    return True
+                   if top > r:
+                       top = r
+                   if bottom < r:
+                       bottom = r
+ 
+        # bottom
+        if R > key[0] + 1:
+           r = key[0] + 1
+           c = key[1]
+           if 0 < buffer[r,c]:
+               k = (r,c)
+               if not k in visited:
+                   S += 1
+                   visited.add(k)
+                   Q.append(k)
 
+                   if top > r:
+                       top = r
+                   if bottom < r:
+                       bottom = r
 
+        # left
+        if 0 < key[1]:
+           r = key[0]
+           c = key[1] - 1
+           if 0 < buffer[r,c]:
+               k = (r,c)
+               if not k in visited:
+                   S += 1
+                   visited.add(k)
+                   Q.append(k)
 
-def get_stat():
-    global dir, fnames, files_num, buffers
+                   if left > c:
+                       left = c
+                   if right < c:
+                       right = c
+ 
+        # right
+        if C > key[1] + 1:
+           r = key[0]
+           c = key[1] + 1
+           if 0 < buffer[r,c]:
+               k = (r,c)
+               if not k in visited:
+                   S += 1
+                   visited.add(k)
+                   Q.append(k)
 
-    path_base = "..\\data\\"
-
-    dicom.VERBOSE = False
-
-    min_rows = 99999
-    min_cols = 99999
-    max_rows = 0
-    max_cols = 0
-    min_frames = 99999
-    max_frames = 0
-
-    for d in [d for d in os.listdir(path_base) if not d.startswith(".")]:
-        path1 = path_base + d + "\\study\\"
-        for d2 in [d for d in os.listdir(path1) if not d.startswith(".")]:
-            path_final = path1 + d2 + "\\"
-            dir = path_final
-
-
-            fnames = [f for f in os.listdir(dir) if "dcm" in f and not f.startswith('.')]
-            files_num = len(fnames)
-
-            for i, f in enumerate(fnames):
-                file_buffer = np.fromfile(dir + f, dtype=np.uint8, sep='')
-                dicom_obj = dicom.process(file_buffer)
-               
-                rows = dicom_obj.Rows
-                cols = dicom_obj.Columns
-
-                if min_rows > rows:
-                    min_rows = rows
-                if min_cols > cols:
-                    min_cols = cols
-                if max_rows < rows:
-                    max_rows = rows
-                if max_cols < cols:
-                    max_cols = cols
-
-            frames = len(fnames)
-       
-            if min_frames > frames:
-                min_frames = frames
-            if max_frames < frames:
-                max_frames = frames
-
-    print "STAT"
-    print " min rows", min_rows
-    print " max rows", max_rows
-    print " min cols", min_cols
-    print " max cols", max_cols
-    print " mim frames", min_frames
-    print " max frames", max_frames
-
-
-
-
-
-
-
-
+                   if left > c:
+                       left = c
+                   if right < c:
+                       right = c
+ 
+    return top, bottom, left, right, S
 
 
 
 
 
-def process_and_save(): 
-    global dir, fnames, files_num, buffers
-
-    path_base = "..\\data\\"
-
-    dicom.VERBOSE = False
-
-    for d in [d for d in os.listdir(path_base) if not d.startswith(".")]:
-        path1 = path_base + d + "\\study\\"
-        for d2 in [d for d in os.listdir(path1) if not d.startswith(".")]:
-            path_final = path1 + d2 + "\\"
-            dir = path_final
-            
-
-            fnames = [f for f in os.listdir(dir) if "dcm" in f and not f.startswith('.')]
-            files_num = len(fnames)
-
-            buffers.clear()
 
 
-            fig = process(ret=True)
+def get_rectangles(buffer):
+    rects = []
+    visited = set()
 
-            tokens = dir.split("\\")
+    R = buffer.shape[0]
+    C = buffer.shape[1]
 
-            fname = "..\\imgs\\%s_%s_%s.png" % (tokens[-4], tokens[-2], tokens[-1])
-            fig.savefig(fname)
-   
+    for r in range(R):
+        for c in range(C):
+            if 0 == buffer[r,c]:
+                continue
+
+            key = (r,c)
+            if key in visited:
+                continue
+
+            top, bottom, left, right, S = detect_rectangle(buffer, r, c, visited)
+            rects.append((top, bottom, left, right, S))
+
+    return rects
 
 
 
 
 
-def process(ret=False): 
-    global dir, fnames, files_num, buffers
 
+
+def cache(case_dir): 
+
+    global BUFFERS
 
     dicom.VERBOSE = False
 
-    fnames = [f for f in os.listdir(dir) if "dcm" in f and not f.startswith('.')]
-    files_num = len(fnames)
+    scan_dirs = [d for d in os.listdir(PATH_BASE + case_dir + "\\study\\") if not d.startswith('.')]
 
-    for i, f in enumerate(fnames):
-        if i not in buffers:
-            img_buffer = dicom.img_fromfile(dir + f)
-            buffers[i] = img_buffer
+    fnames = None
 
+    # caching
+    for d in scan_dirs:
+        print "    %s" % d
+        key = case_dir + "\\study\\" + d 
+        if key in BUFFERS:
+            break
+
+
+        fname_cache = PATH_CACHE + "teach_heart_detector.py.%s.BUFFERS" % key.replace("\\", "_")
+        if os.path.exists(fname_cache):
+            with open(fname_cache, "r") as fin:
+                text = fin.read()
+                BUFFERS[key] = eval(text.replace("array", "np.array"))
+                continue
+
+
+
+        path = PATH_BASE + key + "\\"
+
+        fnames = [f for f in os.listdir(path) if "dcm" in f and not f.startswith('.')]
+
+        frames = []
+
+        for f in fnames:
+            print "        %s" % f
+            fn = path + f
+            file_buffer = np.fromfile(fn, dtype=np.uint8, sep='')
+            dicom_obj = dicom.process(file_buffer)
+
+            vec = np.array([
+                       dicom_obj.Rows,
+                       dicom_obj.Columns,
+                       -1 if dicom_obj.Gender == "F" else 1,
+                       dicom_obj.Age
+                          ], dtype=np.float64)
+
+            frames.append(dicom_obj.img_buffer)
+
+        rects = process(frames)
+
+        BUFFERS[key] = (rects, vec)
+    
+        with open(fname_cache, "w+") as fout:
+            fout.write(str(BUFFERS[key]))
    
      
 
 
-    prev = buffers[0]
-    shape2D = prev.shape
-    shape3D = (shape2D[0], shape2D[1], 1)
 
-    tmp = np.zeros(shape2D, dtype=float)
+
+def process(frames):
+
+    files_num = len(frames)
+
+    prev = frames[0]
+    shape2D = prev.shape
     freq = np.zeros(shape2D, dtype=float)
 
     cnt = 0
@@ -170,8 +225,9 @@ def process(ret=False):
         cnt += 1
         if cnt != thr:
             continue
+
         cnt = 0
-        curr = buffers[i]
+        curr = frames[i]
         #
         tmp = (prev - curr) ** 2
         mv = np.mean(tmp)
@@ -218,82 +274,128 @@ def process(ret=False):
     freq[-10:, :] = 0
 
 
-    for r in range(10, freq.shape[0] - 10):
-        if freq[r,:].sum() > 0:
-            top_r = r
-            break
+    rects = get_rectangles(freq)
+
+    return rects
+
+
+
+
+
+
+
+
+def load_positions():
+    global POSITIONS
+
+    with open('positions.csv', "r") as fin:
+        g = csv.reader(fin, delimiter=',')
+        for tokens in g:
+            #    0    1      2           3        4     5      6    7       8
+            # case, sax, fname, bottom tip, top tip, left, right, top, bottom
+            #print tokens
+            key = "%s\\study\\%s" % (tokens[0], tokens[1])
+            pos = ( int(tokens[7]), int(tokens[8]), int(tokens[5]), int(tokens[6]), 0 )
+            POSITIONS[key] = pos
+
+
+
+
+
+
+
+
+def area_of_intersection(t1, b1, l1, r1,  t2, b2, l2, r2):
+    return max(0, min(r1, r2) - max(l1, l2)) * max(0, min(b1, b2) - max(t1, t2))
+
+def area(t, b, l, r):
+    return (b - t) * (r - l)
+
+
+
+
+def F1(p, r):
+    return 2. * (p * r) / float(p + r)
+
+
+
+
+def prepare_data():
+    global TRAIN_SET, TEST_SET
+
+    keys = BUFFERS.keys()
+    N = len(keys)
+
+    for i in range(N):
+        data = BUFFERS[keys[i]]
+        N += len(data[0])
+
+
+    indices = range(N)
+
+    np.random.shuffle(indices)
+
+    to_train = int(N * .7)
+    train_indices = indices[:to_train]
+    test_indices  = indices[to_train:]
+
+
+    # 5 rect features + 4 img features
+    columns = 9 + 1
+
+    TRAIN_SET = np.zeros((to_train, columns), dtype=np.float64)
+    TEST_SET  = np.zeros((N - to_train, columns), dtype=np.float64)
+
+    idx = 0
+    for i in train_indices:
+        key = keys[i]
+
+        pos = POSITIONS[key]
+        data = BUFFERS[key]
+
+        rects = data[0]
+        img_data = data[1]
+
+        for r in rects:
+            SP = area(pos[0], pos[1], pos[2], pos[3])
+            SR = area(r[0], r[1], r[2], r[3])
+            SI = area_of_intersection(pos[0], pos[1], pos[2], pos[3],  r[0], r[1], r[2], r[3])
+
+            P = F1(float(SI) / SP, float(SI) / SR)
+
+
+            TRAIN_SET[idx,:4] = img_data
+            TRAIN_SET[idx,4:-1] = r
+            TRAIN_SET[idx,-1] = P
+            idx += 1
+
+        
     
-    for r in range(freq.shape[0] - 10, 10, -1):
-        if freq[r,:].sum() > 0:
-            bottom_r = r
-            break
-    
-    for c in range(10, freq.shape[1] - 10):
-        if freq[:,c].sum() > 0:
-            left_c = c
-            break
-    
-    for c in range(freq.shape[1] - 10, 10, -1):
-        if freq[:,c].sum() > 0:
-            right_c = c
-            break
+
+
+
+
+#def teach_LR():
     
 
-    print top_r, bottom_r, left_c, right_c
-
-
-    freq = freq.reshape(shape3D)
-
-
-    plt.clf()
-    ax = plt.subplot(121)
-    plt.imshow(buffers[0])
-    ax.add_patch(patches.Rectangle((left_c, top_r), (right_c - left_c), (bottom_r - top_r), fill=False, linewidth=2, edgecolor='red'))
-
-    plt.subplot(122)
-    plt.imshow(np.concatenate((freq,freq,freq), axis=2))
-
-    if not ret:
-        plt.show()
-    else:
-        return plt.gcf()
 
 
 
 
+def main():
+    np.random.seed()
+
+    load_positions()
+
+    for k in POSITIONS:
+        print k, POSITIONS[k]
+
+    for d in [d for d in os.listdir(PATH_BASE) if not d.startswith(".")]:
+        print "Caching %s" % d
+        cache(d)
 
 
-
-
-
-def main(): 
-    global dir, fnames, files_num, buffers
-
-    dir = sys.argv[1]
-
-    if dir == "ALL":
-        process_and_save()
-        return 0
-
-    if dir == "STAT":
-        get_stat()
-        return 0
-
-    dicom.VERBOSE = False
-
-    fnames = [f for f in os.listdir(dir) if "dcm" in f and not f.startswith('.')]
-    files_num = len(fnames)
-
-    for i, f in enumerate(fnames):
-        if i not in buffers:
-            img_buffer = dicom.img_fromfile(dir + f)
-            buffers[i] = img_buffer
-
-    process() 
-
-
-
-
+    prepare_data()
 
 
 if __name__ == "__main__":
