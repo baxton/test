a077d1e99da15ddedf91e96b2b4a7e3ca659876c
Maxim Alekseykin <baxton-sg@users.noreply.github.com>
2016-01-20 18:56:06 +0800

    utils_wr.py

--- utils.py
+++ utils.py
@@ -2,131 +2,95 @@
 
 import os
 import sys
-import numpy as np
 import ctypes
+import numpy as np
+
+UTILS_DLL = ctypes.cdll.LoadLibrary("utils.dll")
 
 
-SEP = os.path.sep
 
 
-DICOM_DLL = ctypes.cdll.LoadLibrary("dicom.dll")
-
-
-class DICOM(object):
+class UTILS(object):
     def __init__(self):
-        # meta data access offsets
-        self.MDSeriesDescription       = 0
-        self.MDPixelRepresentation     = 1
-        self.MDBitsStored              = 2
-        self.MDBitsAllocated           = 3
-        self.MDColumns                 = 4
-        self.MDRows                    = 5
-        self.MDGender                  = 6
-        self.MDAge                     = 7
-        self.MDLargestPixelValue       = 8
-        self.MDSliceThickness          = 9
-        self.MDFlipAngle               = 10
-        self.MDImagePositionPatient    = 11   # - 13
-        self.MDImageOrientationPatient = 14   # - 19
-
-
-    def fromfile(self, fname):
-        self.fname = fname
-
-        # read row content
-        # print "read file", fname
-        self.buffer = np.fromfile(self.fname, dtype=np.uint8, sep='')
-
-        # process in C++ and get object's handler
-        self.dicom = ctypes.c_void_p(0)
-        DICOM_DLL.dicom_fromfile(ctypes.c_void_p(self.buffer.ctypes.data), 
-                                 ctypes.c_int(self.buffer.shape[0]),
-                                 ctypes.c_void_p(ctypes.addressof(self.dicom)))
-    
-
-    def free(self):
-        DICOM_DLL.dicom_free(self.dicom)
         pass
 
 
+    def get_statistics(self, buffer):
+        tmp = buffer
+        if tmp.dtype != np.float64:
+            tmp = tmp.astype(np.float64)
 
-    def Rows(self):
-        rows = ctypes.c_int(0)
-        DICOM_DLL.dicom_rows(self.dicom,
-                             ctypes.c_void_p(ctypes.addressof(rows)))
-        return rows.value
+        total_size = tmp.shape[0]
+        for s in range(1, len(tmp.shape)):
+            total_size *= tmp.shape[s]
 
+        mv = ctypes.c_double(0)
+        skew = ctypes.c_double(0)
+        var = ctypes.c_double(0)
+        kur = ctypes.c_double(0)
 
-    def Columns(self):
-        columns = ctypes.c_int(0)
-        DICOM_DLL.dicom_columns(self.dicom,
-                                ctypes.c_void_p(ctypes.addressof(columns)))
-        return columns.value
-
-    def img_buffer(self):
-        img_length = ctypes.c_int(0)
-        null = ctypes.c_void_p(0)
-
-        DICOM_DLL.dicom_buffer(self.dicom,
-                               null,
-                               ctypes.c_void_p(ctypes.addressof(img_length)))
-        buffer = np.zeros((img_length.value,), dtype=np.uint16)
-        DICOM_DLL.dicom_buffer(self.dicom,
-                               ctypes.c_void_p(buffer.ctypes.data),
-                               null)
-        buffer = buffer.reshape((self.Rows(), self.Columns()))
-        return buffer
-
-
-    def img_metadata(self):
-        length = ctypes.c_int(0)
-        null = ctypes.c_void_p(0)
- 
-        DICOM_DLL.dicom_metadata(self.dicom,
-                                 null,
-                                 ctypes.c_void_p(ctypes.addressof(length)))
-        buffer = np.zeros((length.value,), dtype=np.float64)
-        DICOM_DLL.dicom_metadata(self.dicom,
-                                 ctypes.c_void_p(buffer.ctypes.data),
-                                 null)
-        return buffer
+        UTILS_DLL.get_statistics(ctypes.c_void_p(tmp.ctypes.data),
+                                 ctypes.c_int(total_size),
+                                 ctypes.c_void_p(ctypes.addressof(mv)),
+                                 ctypes.c_void_p(ctypes.addressof(skew)),
+                                 ctypes.c_void_p(ctypes.addressof(var)),
+                                 ctypes.c_void_p(ctypes.addressof(kur)))
+        return mv, skew, var, kur
 
 
 
-    def verbose(self, val):
-        DICOM_DLL.dicom_verbose(ctypes.c_int(1 if val else 0))
+    def get_frequencies(self, frames, MEAN_MUL, LOW_VAL, HIGH_VAL):
+        rows = frames.shape[1]
+        cols = frames.shape[2]
+        frames_num = frames.shape[0]
+
+        frames = frames.flatten()
+        freq = np.zeros((rows * cols, ), dtype=np.float64)
+
+        UTILS_DLL.get_frequencies(ctypes.c_void_p(frames.ctypes.data),
+                                  ctypes.c_int(rows),
+                                  ctypes.c_int(cols),
+                                  ctypes.c_int(frames_num),
+                                  ctypes.c_void_p(freq.ctypes.data),
+                                  ctypes.c_double(MEAN_MUL),
+                                  ctypes.c_double(LOW_VAL),
+                                  ctypes.c_double(HIGH_VAL))
+        return freq.reshape((rows, cols))
 
 
 
-        
+
+
+
+
 
 
 
 
 
 def main():
-    fname = sys.argv[1]
-    dicom = DICOM() 
+    from dicom_wr import DICOM
+    import scipy.stats as stats
+
+    fname = os.path.join("..", "data", "train", "1", "study", "sax_10", "IM-4562-0001.dcm")
+    print "processing", fname
+
+    dicom = DICOM()
+    dicom.verbose(False)
     dicom.fromfile(fname)
-    #
-    import matplotlib.pyplot as plt
-    plt.clf()
-    plt.imshow(dicom.img_buffer())
-    plt.show()
-
-    metadata = dicom.img_metadata()
-    print metadata
-    print metadata.shape 
-
-    print "Rows", metadata[dicom.MDRows]
-    print "ImageOrientationPatient", metadata[dicom.MDImageOrientationPatient], metadata[dicom.MDImageOrientationPatient+1], metadata[dicom.MDImageOrientationPatient+2], metadata[dicom.MDImageOrientationPatient+3], metadata[dicom.MDImageOrientationPatient+4], metadata[dicom.MDImageOrientationPatient+5]
-
-    #
+    buffer = dicom.img_buffer()
     dicom.free()
+    dicom = None
+
+    utils = UTILS()
+    mv, skew, var, kur = utils.get_statistics(buffer)
+
+    print mv.value, skew.value, var.value, kur.value
+    print np.mean(buffer), stats.skew(buffer.flatten()), np.var(buffer), stats.kurtosis(buffer.flatten())
 
 
 
 
 if __name__ == "__main__":
-    main()     
-
+    main()
+     
